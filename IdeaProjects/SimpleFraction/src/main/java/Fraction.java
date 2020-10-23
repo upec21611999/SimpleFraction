3f3fbad6eef397b210d60f8aea7a96a3638a18ce
upec21611999 <secours.1@hotmail.com>
2020-10-23 11:56:00 +0200

    firt commit

---
+++ IdeaProjects/SimpleFraction/src/main/java/Fraction.java
@@ -0,0 +1,15 @@
+public class Fraction {
+    private int num;
+    private int den;
+
+    public Fraction (int num, int den){
+        this.num = num;
+        this.den = den;
+    }
+
+    @Override
+    public String toString() {
+        return num + "/" + den ;
+    }
+
+}
---
+++ IdeaProjects/SimpleFraction/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        Fraction F = new Fraction(3,5);
+        System.out.print(F.toString());
+    }
+
+
+}
